ac3cd6952b19479fb559a28d00c666591df4b36a
unknown <branson8815@gmail.com>
2024-12-07 18:43:09 -0800

    added functionality to some of the keys

--- static/sandbox.js
+++ static/sandbox.js
@@ -1,4 +1,17 @@
 function displaytext(letter){
 
-    console.log("Hello");
+    var output = document.getElementById(letter).innerHTML;
+    // console.log(output);
+
+    document.getElementById("lettercombo").value += output;
+}
+
+function logtext(values){
+    var list_values = [];
+
+    
+}
+
+function log_test(values){
+    console.log(values);
 }
