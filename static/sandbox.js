286557c06623e2b77caef53d721b686b75009914
branson8815 <96606008+branson8815@users.noreply.github.com>
2024-12-08 15:30:46 -0800

    minor changes with all of the html

--- static/sandbox.js
+++ static/sandbox.js
@@ -15,3 +15,19 @@ function logtext(values){
 function log_test(values){
     console.log(values);
 }
+
+
+const alphabet = document.getElementsByClassName('alphabet');
+
+// for(i = 0; i< alphabet.length; i++){
+//     console.log(alphabet[i].innerHTML);
+//     // console.log("hello")
+// }
+
+
+// for(i=0; i<5; i++){
+//     console.log(i);
+// }
+
+console.log(alphabet)
+console.log(alphabet[0])
